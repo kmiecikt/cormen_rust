c3bdbb26b5798f2da4305bbd5fa5886ce7a28d74
Tomasz Kmiecik <tomaskmi@microsoft.com>
2022-06-16 18:34:10 +0200

    List: adding iterator

--- src/list.rs
+++ src/list.rs
@@ -26,8 +26,25 @@ impl<T> List<T> {
         })
     }
     
-    pub fn peek(self: &List<T>) -> Option<&T> {
+    pub fn peek(&self) -> Option<&T> {
         self.head.as_ref().map(|head_node| &head_node.value)
+    }
+    
+    pub fn reverse(&mut self) {
+        let mut current = self.head.take();
+        let mut previous = None;
+        
+        while let Some(mut current_node) = current {
+            current = current_node.next.take();
+            current_node.next = previous;
+            previous = Some(current_node);
+        }
+        
+        self.head = previous;
+    }
+    
+    pub fn iter<'a>(&'a self) -> ListIterator<'a, T> {
+        ListIterator { current: &self.head }
     }
 }
 
@@ -35,6 +52,21 @@ impl<T> Drop for List<T> {
     fn drop(&mut self) {
         while let Some(_) = self.pop() {
         }
+    }
+}
+
+pub struct ListIterator<'a, T> {
+    current: &'a Link<T>
+}
+
+impl<'a, T> Iterator for ListIterator<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.current.as_ref().map(|current_node| {
+            self.current = &current_node.next;
+            &current_node.as_ref().value
+        })
     }
 }
 
@@ -67,5 +99,35 @@ mod tests {
         assert_eq!(Some(2), list.pop());
         assert_eq!(Some(1), list.pop());
         assert_eq!(None, list.pop());
+    }
+    
+    #[test]
+    fn test_reverse() {
+        let mut list = List::new();
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+        list.reverse();
+        
+        assert_eq!(Some(1), list.pop());
+        assert_eq!(Some(2), list.pop());
+        assert_eq!(Some(3), list.pop());
+        assert_eq!(None, list.pop());
+    }
+   
+    #[test]
+    fn iter_tests() {
+        let mut list = List::new();
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+        
+        let mut iterator = list.iter();
+        assert_eq!(3, *iterator.next().unwrap());
+        assert_eq!(2, *iterator.next().unwrap());
+        assert_eq!(1, *iterator.next().unwrap());
+        assert_eq!(None, iterator.next());
     }
 }
